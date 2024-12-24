c6b8bf9ea7dee8894dc23e89a6c32ebf0b4fce0f
grimlokason <youenn.gestin@grim-is-a-geek.fr>
2024-12-24 14:21:20 +0100

    Update BleCompositeHID.cpp

--- BleCompositeHID.cpp
+++ BleCompositeHID.cpp
@@ -7,7 +7,6 @@
 #include <driver/adc.h>
 #include "sdkconfig.h"
 
-
 #include "BleCompositeHID.h"
 #include "BleConnectionStatus.h"
 
@@ -62,7 +61,7 @@ BleCompositeHID::BleCompositeHID(std::string deviceName, std::string deviceManuf
     this->deviceName = deviceName.substr(0, CONFIG_BT_NIMBLE_GAP_DEVICE_NAME_MAX_LEN - 1);
     this->deviceManufacturer = deviceManufacturer;
     this->batteryLevel = batteryLevel;
-    this->_connectionStatus = new BleConnectionStatus();   
+    this->_connectionStatus = new BleConnectionStatus();
 }
 
 BleCompositeHID::~BleCompositeHID()
